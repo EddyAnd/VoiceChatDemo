8a598e02b92a6ba0c397b3f2cdc72e892426c3c2
wangzhefan <532606582@qq.com>
2019-03-18 13:53:02 +0800

    Macbook Test

--- app/src/main/java/com/example/vociechatdemo/MainActivity.java
+++ app/src/main/java/com/example/vociechatdemo/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.example.vociechatdemo;
 
 import android.support.v7.app.AppCompatActivity;
@@ -5,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    public int age = 29;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
